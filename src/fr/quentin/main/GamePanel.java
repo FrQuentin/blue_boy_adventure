9b315836873a176134c89a7d5006c3c1e5c75d73
FrQuentin <fouilleul.quentin@outlook.fr>
2022-05-15 14:46:35 +0200

    0.5

--- src/fr/quentin/main/GamePanel.java
+++ src/fr/quentin/main/GamePanel.java
@@ -18,13 +18,20 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol; // 768 Pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 Pixels
 
+    // World Settings
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+
     // Fps
     int fps = 60;
 
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
 
     public GamePanel() {
